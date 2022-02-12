856d8be2e8dd453d20b5c67195a5ec2f346ae658
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-12 12:35:45 +0300

    Added Register User function

--- Awwwards/views.py
+++ Awwwards/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .Threading import send_activation_email
 
 # Create your views here.
 def Home(request):
     return render(request, 'Index.html')
+
+def Register(request):
+    if request.method == 'POST':
+        context = {'has_error': False}
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.error(request, '⚠️ Passwords Do Not Match! Try Again')
+            return redirect('Register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, '⚠️ Username Already Exists! Choose Another One')
+            return redirect('Register')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, '⚠️ Email Address Already Exists! Choose Another One')
+            return redirect('Register')
+
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email)
+        user.set_password(password1)
+        user.is_active = False
+        user.save()
+
+        if not context['has_error']:
+            send_activation_email(user, request)
+            messages.success(request, '✅ Regristration Successful! An Activation Link Has Been Sent To Your Email')
+            return redirect('Register')
+
+    return render(request, 'Register.html')
