b53155bf08ea2d52732cc5aac1ba152c16c76a30
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-12 13:09:42 +0300

    Added Residter Page & styling

--- Awwwards/urls.py
+++ Awwwards/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 from Core import settings
 
 urlpatterns = [
-    path('', views.Home, name="Home")
+    path('', views.Home, name="Home"),
+    path('register', views.Register, name="Register"),
+    path('login', views.Login, name="Login")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
