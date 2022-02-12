588e9f5667891e33e6c5addb8fae7c55ce0145b5
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-12 13:24:30 +0300

    Added Password Reset URLs

--- Awwwards/urls.py
+++ Awwwards/urls.py
@@ -3,9 +3,15 @@ from Awwwards import views
 from django.urls import path
 from django.conf.urls.static import static
 from Core import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.Home, name="Home"),
     path('register', views.Register, name="Register"),
-    path('login', views.Login, name="Login")
+    path('login', views.Login, name="Login"),
+    path('resetpassword/',auth_views.PasswordResetView.as_view(template_name='ResetPassword.html'), name = 'reset_password'),
+    path('resetpassword/sent/',auth_views.PasswordResetDoneView.as_view(template_name='PasswordResetSent.html'), name = 'password_reset_done'),
+    path('resetpassword/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='PasswordResetConfirm.html'), name = 'password_reset_confirm'),
+    path('resetpassword/success/',auth_views.PasswordResetCompleteView.as_view(template_name='PasswordResetSuccess.html'), name = 'password_reset_complete'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
