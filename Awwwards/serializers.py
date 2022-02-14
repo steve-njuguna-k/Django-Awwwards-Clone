c6f6a0bac24579ad384b4a1d073c65d70e77691f
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-15 02:11:23 +0300

    Added serializer.py

---
+++ Awwwards/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Profile, Portfolio
+
+class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+    first_name = serializers.ReadOnlyField(source='user.first_name')
+    last_name = serializers.ReadOnlyField(source='user.last_name')
+    email = serializers.ReadOnlyField(source='user.email')
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class PortfolioSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='author.username')
+    first_name = serializers.ReadOnlyField(source='author.first_name')
+    last_name = serializers.ReadOnlyField(source='author.last_name')
+    email = serializers.ReadOnlyField(source='author.email')
+
+    class Meta:
+        model = Portfolio
+        fields = '__all__'
--- Core/settings.py
+++ Core/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
 
     'Awwwards',
     'django_cleanup.apps.CleanupConfig',
-    'cloudinary'
+    'cloudinary',
+    'restframework',
 ]
 
 MIDDLEWARE = [
